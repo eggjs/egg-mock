6ddbda14cd3c8758b223d239c7433e3bccdf0c53
fengmk2 <m@fengmk2.com>
2017-02-08 16:53:47 +0800

    test: remove unuse codes (#26)

--- test/fixtures/apps/env-app/config/config.default.js
+++ test/fixtures/apps/env-app/config/config.default.js
@@ -4,10 +4,6 @@ exports.fakeplugin = {
   foo: 'bar-default',
 };
 
-// exports.alipayuserservice = {
-//   authcenterUrl: 'http://authcenter.stable.alipay.net',
-// };
-
 exports.logger = {
   consoleLevel: 'NONE',
 };
