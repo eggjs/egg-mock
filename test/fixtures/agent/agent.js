39abcd75eb3570fe1aff6dc97546be7800f1f680
popomore <sakura9515@gmail.com>
2017-03-02 13:12:10 +0800

    f

--- test/fixtures/agent/agent.js
+++ test/fixtures/agent/agent.js
@@ -2,11 +2,13 @@
 
 const fs = require('fs');
 const path = require('path');
+const mkdirp = require('mkdirp');
 const Client = require('./client');
 
 module.exports = function(agent) {
   const done = agent.readyCallback('agent:agent');
   const p = path.join(__dirname, 'run/test.txt');
+  mkdirp.sync(path.join(__dirname, 'run'));
   fs.writeFile(p, '123', done);
 
   agent.client = agent.cluster(Client).create();
