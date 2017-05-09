24a49300942cea9edfa6d19ba8d0a094a10ee336
fengmk2 <fengmk2@gmail.com>
2017-05-09 19:14:04 +0800

    f

--- app.js
+++ app.js
@@ -1,77 +1,11 @@
 'use strict';
 
-const debug = require('debug')('egg-mock:app');
-
-const MOCK_REQUEST_PATH = '/__egg_mock_call_function';
-
 module.exports = app => {
-  app.beforeStart(() => {
-    // https://eggjs.org/zh-cn/core/security.html#%E5%BF%BD%E7%95%A5-json-%E8%AF%B7%E6%B1%82
-    // ignore JSON request
-    if (app.config.security && app.config.security.csrf.enable) {
-      if (typeof app.config.security.csrf.matching === 'function') {
-        const matching = app.config.security.csrf.matching;
-        app.config.security.csrf.matching = (...args) => {
-          const ctx = args[0];
-          if (ctx.path === MOCK_REQUEST_PATH) return false;
-          return matching(...args);
-        };
-      }
-    }
-    app.post(MOCK_REQUEST_PATH, handleRequest);
-  });
+  // make sure clusterAppMock position before securities
+  const index = app.config.coreMiddleware.indexOf('securities');
+  if (index >= 0) {
+    app.config.coreMiddleware.splice(index, 0, 'clusterAppMock');
+  } else {
+    app.config.coreMiddleware.push('clusterAppMock');
+  }
 };
-
-function* handleRequest() {
-  const { method, args } = this.request.body;
-  if (!method) {
-    this.status = 422;
-    this.body = {
-      success: false,
-      error: 'Missing method',
-    };
-    return;
-  }
-  if (args && !Array.isArray(args)) {
-    this.status = 422;
-    this.body = {
-      success: false,
-      error: 'args should be an Array instance',
-    };
-    return;
-  }
-  if (typeof this.app[method] !== 'function') {
-    this.status = 422;
-    this.body = {
-      success: false,
-      error: `method "${method}" not exists on app`,
-    };
-    return;
-  }
-
-  debug('call %s with %j', method, args);
-
-  for (let i = 0; i < args.length; i++) {
-    const arg = args[i];
-    if (arg && typeof arg === 'object') {
-      // convert __egg_mock_type back to function
-      if (arg.__egg_mock_type === 'function') {
-        // eslint-disable-next-line
-        args[i] = eval(`(function() { return ${arg.value} })()`);
-      } else if (arg.__egg_mock_type === 'error') {
-        const err = new Error(arg.message);
-        err.name = arg.name;
-        err.stack = arg.stack;
-        for (const key in arg) {
-          if (key !== 'name' && key !== 'message' && key !== 'stack' && key !== '__egg_mock_type') {
-            err[key] = arg[key];
-          }
-        }
-        args[i] = err;
-      }
-    }
-  }
-
-  this.app[method](...args);
-  this.body = { success: true };
-}
---
+++ test/fixtures/disable-security/app/context.js
@@ -0,0 +1,7 @@
+module.exports = {
+  getResult: function (result) {
+    return {
+      body: result
+    };
+  }
+};
