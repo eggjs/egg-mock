ba2d6feb4a75754ede8a8f77f38530b23973f862
fengmk2 <fengmk2@gmail.com>
2017-05-11 12:28:22 +0800

    fix: ignore all error on cluster mock restore
    closes https://github.com/eggjs/egg/issues/888

--- test/mock_cluster_restore.test.js
+++ test/mock_cluster_restore.test.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const sleep = require('mz-modules/sleep');
 const mm = require('..');
 
-describe('test/mock_cluster_restore.test.js', () => {
+const fixtures = path.join(__dirname, 'fixtures');
+
+describe.only('test/mock_cluster_restore.test.js', () => {
   let app;
   before(() => {
     app = mm.cluster({
@@ -34,5 +40,28 @@ describe('test/mock_cluster_restore.test.js', () => {
     yield app.httpRequest()
       .get('/session')
       .expect({});
+  });
+
+  describe('handle uncaughtException', () => {
+    let app;
+    before(() => {
+      app = mm.cluster({
+        baseDir: 'apps/app-throw',
+      });
+      return app.ready();
+    });
+    after(() => app.close());
+
+    it('should handle uncaughtException and log it', function* () {
+      yield app.httpRequest()
+        .get('/throw')
+        .expect('foo')
+        .expect(200);
+
+      yield sleep(1100);
+      const logfile = path.join(fixtures, 'apps/app-throw/logs/app-throw/common-error.log');
+      const body = fs.readFileSync(logfile, 'utf8');
+      assert(body.includes('ReferenceError: a is not defined (uncaughtException throw'));
+    });
   });
 });
