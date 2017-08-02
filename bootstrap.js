5ae8fcf13af63451ba950434db2c6ca93b38b767
dead-horse <dead_horse@qq.com>
2017-08-02 17:46:40 +0800

    f

--- bootstrap.js
+++ bootstrap.js
@@ -1,17 +1,18 @@
 'use strict';
 
 const assert = require('power-assert');
-const mm = require('./index');
+const mock = require('./index');
 
 const options = {};
 if (process.env.EGG_BASE_DIR) options.baseDir = process.env.EGG_BASE_DIR;
-const app = mm.app(options);
+const app = mock.app(options);
 
 before(() => app.ready());
-afterEach(mm.restore);
+afterEach(mock.restore);
 
 module.exports = {
   assert,
   app,
-  mm,
+  mock,
+  mm: mock,
 };
--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -2,13 +2,19 @@
 
 const path = require('path');
 const baseDir = process.env.EGG_BASE_DIR = path.join(__dirname, './fixtures/app');
-const { app, assert, mm } = require('../bootstrap');
+const { app, assert, mm, mock } = require('../bootstrap');
 
 describe('test/bootstrap.test.js', () => {
   it('should create app success', () => {
     assert(app.baseDir === baseDir);
+  });
+
+  it('should mock and mm success', () => {
+    assert(app.baseDir === baseDir);
     mm(app, 'baseDir', 'foo');
     assert(app.baseDir === 'foo');
+    mock(app, 'baseDir', 'bar');
+    assert(app.baseDir === 'bar');
   });
 
   it('should afterEach(mm.restore) success', () => {
