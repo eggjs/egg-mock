f1820d70f2e266d4b18fb7062976b4c14952a16f
fengmk2 <fengmk2@gmail.com>
2018-02-28 23:48:04 +0800

    feat: mm.app() support server event (#65)
    https://github.com/eggjs/egg/pull/2146#issuecomment-369189329

--- test/fixtures/app/app/router.js
+++ test/fixtures/app/app/router.js
@@ -1,7 +1,13 @@
 'use strict';
 
-module.exports = function(app) {
+module.exports = app => {
   app.get('/', function* () {
     this.body = 'foo';
+  });
+
+  app.get('/keepAliveTimeout', function* () {
+    this.body = {
+      keepAliveTimeout: this.app.serverKeepAliveTimeout,
+    };
   });
 };
