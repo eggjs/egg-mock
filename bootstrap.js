79c90bacb2c3915c6a1dbc9ae5ee1030b6677183
fengmk2 <fengmk2@gmail.com>
2018-08-07 11:16:26 +0800

    f

--- bootstrap.js
+++ bootstrap.js
@@ -8,11 +8,9 @@ if (process.env.EGG_BASE_DIR) options.baseDir = process.env.EGG_BASE_DIR;
 const app = mock.app(options);
 
 before(() => app.ready());
-afterEach(() => {
-  // restore should be the last one
-  return app.backgroundTasksFinished()
-    .then(() => mock.restore());
-});
+afterEach(() => app.backgroundTasksFinished());
+// restore should be the last one
+afterEach(mock.restore);
 
 module.exports = {
   assert,
