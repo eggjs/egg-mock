01bda07b5c053479d6786ebd63095780aaa6202b
fengmk2 <fengmk2@gmail.com>
2018-12-18 19:26:58 +0800

    feat: bootstrap support run on jest env
    before => beforeAll

--- bootstrap.js
+++ bootstrap.js
@@ -7,7 +7,13 @@ const options = {};
 if (process.env.EGG_BASE_DIR) options.baseDir = process.env.EGG_BASE_DIR;
 const app = mock.app(options);
 
-before(() => app.ready());
+if (typeof beforeAll === 'function') {
+  // jest
+  beforeAll(() => app.ready());
+} else {
+  // mocha
+  before(() => app.ready());
+}
 afterEach(() => app.backgroundTasksFinished());
 // restore should be the last one
 afterEach(mock.restore);
