90b34f0241c91c1122e4fcfeea50264a28aa86c8
killagu <killa123@126.com>
2019-04-29 14:35:06 +0800

    f

--- test/format_options.test.js
+++ test/format_options.test.js
@@ -116,7 +116,6 @@ describe('test/format_options.test.js', () => {
 
   it('should throw when no eggPlugin and options.plugin === true', () => {
     const baseDir = path.join(__dirname, 'fixtures/plugin_throw');
-    const packageJson = path.join(baseDir, 'package.json');
     mm(process, 'cwd', () => {
       return baseDir;
     });
@@ -124,7 +123,7 @@ describe('test/format_options.test.js', () => {
       formatOptions({
         plugin: true,
       });
-    }, new RegExp(`should set eggPlugin in ${packageJson}`));
+    }, /should set eggPlugin in/);
   });
 
   it('should mock process.env.HOME when EGG_SERVER_ENV is default, test, prod', () => {
