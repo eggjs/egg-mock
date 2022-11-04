22f508cba2f6330172db89efde3a678ed35c5258
killa <killa123@126.com>
2022-11-04 22:14:53 +0800

    feat: impl parallel app for mocha parallel mode (#130)
    - use mochaGlobalSetup/mochaGlobalTeardown to setup
    agent in mocha master process.
    - bootstrap app without agent to aviod port conflict.

---
+++ test/parallel.test.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const agentRegister = require('../lib/parallel/agent_register');
+
+describe('test/parallel.test.js', () => {
+  before(async () => {
+    await agentRegister.mochaGlobalSetup();
+    process.env.ENABLE_MOCHA_PARAELLEL = 'true';
+    process.env.AUTO_AGENT = 'true';
+    process.env.EGG_BASE_DIR = path.join(__dirname, './fixtures/apps/foo');
+  });
+
+  after(async () => {
+    await agentRegister.mochaGlobalTeardown();
+    delete process.env.ENABLE_MOCHA_PARAELLEL;
+    delete process.env.AUTO_AGENT;
+    delete process.env.EGG_BASE_DIR;
+  });
+
+  it('should work', async () => {
+    const { app } = require('../bootstrap');
+    await app.ready();
+    await app.httpRequest()
+      .get('/')
+      .expect(200)
+      .expect('foo');
+    await app.close();
+  });
+});
