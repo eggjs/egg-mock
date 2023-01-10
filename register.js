880c282481024005456074a123b4b1f185971f4c
fengmk2 <fengmk2@gmail.com>
2023-01-10 09:30:54 +0800

    fix: ignore bootstrap error on non egg project (#142)

--- register.js
+++ register.js
@@ -9,17 +9,28 @@ exports.mochaGlobalTeardown = async () => {
   await agent.closeAgent();
 };
 
+let _inited = false;
+let _app;
 exports.mochaHooks = {
   async beforeAll() {
-    const { app } = require('./bootstrap');
-    if (app) {
-      await app.ready();
+    if (!_inited) {
+      _inited = true;
+      try {
+        const { app } = require('./bootstrap');
+        _app = app;
+      } catch {
+        // ignore require error
+        // it will throw error on non egg project, e.g.: Error: egg is not found in /foo/bar
+        return;
+      }
+      if (_app) {
+        await _app.ready();
+      }
     }
   },
   async afterEach() {
-    const { app } = require('./bootstrap');
-    if (app) {
-      await app.backgroundTasksFinished();
+    if (_app) {
+      await _app.backgroundTasksFinished();
     }
     await mock.restore();
   },
