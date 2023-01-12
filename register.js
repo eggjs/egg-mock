d4981830794753594cc6e67b7084440bc401a322
killagu <killa123@126.com>
2023-01-12 17:11:14 +0800

    fix: only await app ready when app exists

--- register.js
+++ register.js
@@ -1,44 +1,34 @@
 const debug = require('util').debuglog('egg-mock:register');
 const mock = require('./index').default;
-const agent = require('./lib/agent');
+const agentHandler = require('./lib/agent_handler');
+const appHandler = require('./lib/app_handler');
 
 exports.mochaGlobalSetup = async () => {
   debug('mochaGlobalSetup, agent.setupAgent() start');
-  await agent.setupAgent();
+  await agentHandler.setupAgent();
   debug('mochaGlobalSetup, agent.setupAgent() end');
 };
 
 exports.mochaGlobalTeardown = async () => {
   debug('mochaGlobalTeardown, agent.closeAgent() start');
-  await agent.closeAgent();
+  await agentHandler.closeAgent();
   debug('mochaGlobalTeardown, agent.closeAgent() end');
 };
 
-let _inited = false;
-let _app;
 exports.mochaHooks = {
   async beforeAll() {
-    debug('mochaHooks.beforeAll call, _inited: %s, _app: %s', _inited, !!_app);
-    if (!_inited) {
-      _inited = true;
-      try {
-        const { app } = require('./bootstrap');
-        _app = app;
-      } catch (err) {
-        // ignore require error
-        // it will throw error on non egg project, e.g.: Error: egg is not found in /foo/bar
-        debug('require bootstrap app error: %s', err);
-        return;
-      }
-      if (_app) {
-        await _app.ready();
-      }
+    const app = appHandler.getApp();
+    debug('mochaHooks.beforeAll call, _app: %s', app);
+
+    if (app) {
+      await app.ready();
     }
   },
   async afterEach() {
-    debug('mochaHooks.afterEach call, _inited: %s, _app: %s', _inited, !!_app);
-    if (_app) {
-      await _app.backgroundTasksFinished();
+    const app = appHandler.getApp();
+    debug('mochaHooks.afterEach call, _app: %s', app);
+    if (app) {
+      await app.backgroundTasksFinished();
     }
     await mock.restore();
   },
