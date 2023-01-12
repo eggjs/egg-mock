f4ed458441449da70fd4108747377b7890dc08fb
killa <killa123@126.com>
2023-01-12 20:41:10 +0800

    fix: only await app ready when app exists (#145)

--- bootstrap.js
+++ bootstrap.js
@@ -1,9 +1,8 @@
-const debug = require('util').debuglog('egg-mock:bootstrap');
 const assert = require('assert');
 const path = require('path');
 const mock = require('./index').default;
-const { setupAgent } = require('./lib/agent');
-const mockParallelApp = require('./lib/parallel/app');
+const appHandler = require('./lib/app_handler');
+
 const { getEggOptions } = require('./lib/utils');
 
 const options = getEggOptions();
@@ -12,33 +11,13 @@ const options = getEggOptions();
 const pkgInfo = require(path.join(options.baseDir || process.cwd(), 'package.json'));
 if (pkgInfo.eggPlugin) throw new Error('DO NOT USE bootstrap to test plugin');
 
-let app;
-debug('env.ENABLE_MOCHA_PARALLEL: %s, process.env.AUTO_AGENT: %s',
-  process.env.ENABLE_MOCHA_PARALLEL, process.env.AUTO_AGENT);
-if (process.env.ENABLE_MOCHA_PARALLEL && process.env.AUTO_AGENT) {
-  // setup agent first
-  app = mockParallelApp({
-    ...options,
-    beforeInit: async _app => {
-      const agent = await setupAgent();
-      _app.options.clusterPort = agent.options.clusterPort;
-      debug('mockParallelApp beforeInit get clusterPort: %s', _app.options.clusterPort);
-    },
-  });
-  debug('mockParallelApp app: %s', !!app);
-} else {
-  app = mock.app(options);
-  if (typeof beforeAll === 'function') {
-    // jest
-    beforeAll(() => app.ready());
-    afterEach(() => app.backgroundTasksFinished());
-    afterEach(mock.restore);
-  }
-}
+appHandler.setupApp();
 
 module.exports = {
   assert,
-  app,
+  get app() {
+    return appHandler.getApp();
+  },
   mock,
   mm: mock,
 };
