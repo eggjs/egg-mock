d6eec431fda7c9d796a76a0ea8a41eeb2397efaf
fengmk2 <fengmk2@gmail.com>
2024-07-05 20:50:53 +0800

    fix: should run restore in the current event loop

--- test/mock_env.test.js
+++ test/mock_env.test.js
@@ -1,12 +1,10 @@
-'use strict';
-
-const path = require('path');
-const assert = require('assert');
+const path = require('node:path');
+const { strict: assert } = require('node:assert');
 const mm = require('..');
+
 const fixtures = path.join(__dirname, 'fixtures');
 
 describe('test/mock_env.test.js', () => {
-
   let app;
   before(() => {
     app = mm.app({
@@ -17,9 +15,33 @@ describe('test/mock_env.test.js', () => {
   after(() => app.close());
   afterEach(mm.restore);
 
-  it('should mock env succes', () => {
+  it('should mock env success', () => {
     app.mockEnv('prod');
-    assert(app.config.env === 'prod');
-    assert(app.config.serverEnv === 'prod');
+    assert.equal(app.config.env, 'prod');
+    assert.equal(app.config.serverEnv, 'prod');
+  });
+
+  it('should keep mm.restore execute in the current event loop', async () => {
+    mm.restore();
+    assert.equal(app.config.env, 'unittest');
+    assert.equal(app.config.serverEnv, undefined);
+
+    app.mockEnv('prod');
+    assert.equal(app.config.env, 'prod');
+    assert.equal(app.config.serverEnv, 'prod');
+
+    await sleep(1);
+    assert.equal(app.config.env, 'prod');
+    assert.equal(app.config.serverEnv, 'prod');
+
+    mm.restore();
+    assert.equal(app.config.env, 'unittest');
+    assert.equal(app.config.serverEnv, undefined);
   });
 });
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
